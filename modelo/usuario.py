c18725c8b4430ddced40fa07000a05949483c891
Joel Partida <partidajoel.mail@gmail.com>
2023-10-30 19:20:21 -0300

    agrego validacion por archivos

--- modelo/usuario.py
+++ modelo/usuario.py
@@ -1,24 +1,31 @@
 # Lista de usuarios
+import csv
+import os
+
+
 usuarios = []
 id_usuario = 1  # Variable para asignar IDs únicos a los usuarios
 
 # Crea un nuevo usuario
 def inicializar_usuarios():
     global id_usuario
-    # Agrega un usuario a la lista con un ID único
-    usuarios.append({
-        "id": id_usuario,
-        "nombre de usuario": "juan",
-        "contraseña": "12343",
-    })
-    id_usuario += 1
-    # Agrega un usuario a la lista con un ID único
-    usuarios.append({
-        "id": id_usuario,
-        "nombre de usuario": "pedro",
-        "contraseña": "sads122",
-    })
-    id_usuario += 1
+    if os.path.exists('usuarios.csv'):
+        id_usuario=importar_desde_csv()
+    else:
+        # Agrega un usuario a la lista con un ID único
+        usuarios.append({
+            "id": id_usuario,
+            "nombre de usuario": "juan",
+            "contraseña": "12343",
+        })
+        id_usuario += 1
+        # Agrega un usuario a la lista con un ID único
+        usuarios.append({
+            "id": id_usuario,
+            "nombre de usuario": "pedro",
+            "contraseña": "sads122",
+        })
+        id_usuario += 1
 
 # Crea un nuevo usuario
 def crear_usuario(nombre_usuario, contraseña):
@@ -30,6 +37,7 @@ def crear_usuario(nombre_usuario, contraseña):
         "contraseña": contraseña,
     })
     id_usuario += 1
+    exportar_a_csv()
     # Devuelve el usuario recién creado
     return usuarios[-1]
 
@@ -55,6 +63,7 @@ def editar_usuario_por_id(id_usuario, nuevo_nombre_usuario, nueva_contraseña):
         if usuario["id"] == id_usuario:
             usuario["nombre de usuario"] = nuevo_nombre_usuario
             usuario["contraseña"] = nueva_contraseña
+            exportar_a_csv()
             return usuario
     # Devuelve None si no se encuentra el usuario
     return None
@@ -64,3 +73,27 @@ def eliminar_usuario_por_id(id_usuario):
     global usuarios
     # Crea una nueva lista sin el usuario a eliminar
     usuarios = [usuario for usuario in usuarios if usuario["id"] != id_usuario]
+    exportar_a_csv()
+# Exportar datos a un archivo CSV
+def exportar_a_csv():
+    with open('usuarios.csv', 'w', newline='') as csvfile:
+        campo_nombres = ['id', 'nombre de usuario', 'contraseña']
+        writer = csv.DictWriter(csvfile, fieldnames=campo_nombres)
+        writer.writeheader()
+        for usuario in usuarios:
+            writer.writerow(usuario)
+
+# Importar datos desde un archivo CSV
+def importar_desde_csv():
+    global usuarios
+    usuarios = []  # Limpiamos la lista de usuarios antes de importar desde el archivo CSV
+    with open('usuarios.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            # Convertimos el ID de cadena a entero
+            row['id'] = int(row['id'])
+            usuarios.append(row) 
+    if len(usuarios)>0:
+        return usuarios[-1]["id"]+1
+    else:
+        return 1
